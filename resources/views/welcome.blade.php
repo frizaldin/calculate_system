d95ad2c08a0bd6e085ba0ee1a181d721103fa0cb
unknown <firdanrizaldin7@gmail.com>
2025-07-13 22:17:36 +0700

    Welcome Setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1574,8 +1574,8 @@
                             </a>
                         </span>
                     </li>
-                    /* <li
-                        class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A] before:bottom-1/2 before:top-0 before:left-[0.4rem] before:absolute">
+                    <li class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A] before:bottom-1/2 before:top-0 before:left-[0.4rem] before:absolute"
+                        style="display: none;">
                         <span class="relative py-1 bg-white dark:bg-[#161615]">
                             <span
                                 class="flex items-center justify-center rounded-full bg-[#FDFDFC] dark:bg-[#161615] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] w-3.5 h-3.5 border dark:border-[#3E3E3A] border-[#e3e3e0]">
@@ -1594,7 +1594,7 @@
                                 </svg>
                             </a>
                         </span>
-                    </li> */
+                    </li>
                 </ul>
                 <ul class="flex gap-3 text-sm leading-normal">
                     <li>
