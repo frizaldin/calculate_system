0a36c374e890c9646fb0f4c5b05364ec21d6c978
unknown <firdanrizaldin7@gmail.com>
2025-07-13 22:23:32 +0700

    Welcome Setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1564,7 +1564,7 @@
                         <span>
                             Wishlist
                             <a href="{{ url('wishlist') }}" target="_blank"
-                                class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
+                                class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-3">
                                 <span>Kunjungi</span>
                                 <svg width="10" height="11" viewBox="0 0 10 11" fill="none"
                                     xmlns="http://www.w3.org/2000/svg" class="w-2.5 h-2.5">
@@ -1585,8 +1585,8 @@
                         <span>
                             Website
                             <a href="{{ url('/') }}" target="_blank"
-                                class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
-                                <span>Laracasts</span>
+                                class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-3">
+                                <span>Kunjungi</span>
                                 <svg width="10" height="11" viewBox="0 0 10 11" fill="none"
                                     xmlns="http://www.w3.org/2000/svg" class="w-2.5 h-2.5">
                                     <path d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001" stroke="currentColor"
@@ -1598,7 +1598,7 @@
                 </ul>
                 <ul class="flex gap-3 text-sm leading-normal">
                     <li>
-                        <a href="https://cloud.laravel.com" target="_blank"
+                        <a href="{{ url('/') }}" target="_blank"
                             class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
                             Deploy now
                         </a>
