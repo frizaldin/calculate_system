aa3ea9381e1b324b4cd262e0276cab3108331f95
unknown <firdanrizaldin7@gmail.com>
2025-07-16 21:36:01 +0700

    Ngerapihin Welcome

--- system/resources/views/welcome.blade.php
+++ system/resources/views/welcome.blade.php
@@ -63,7 +63,7 @@
                             </span>
                         </span>
                         <span>
-                            Wishlist
+                            Sistem
                             <a href="{{ url('signin') }}" style="margin-left: 10px" target="_blank"
                                 class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-3">
                                 <span>Kunjungi</span>
@@ -87,7 +87,31 @@
                             Website
                             <a href="{{ url('/') }}" style="margin-left: 10px" target="_blank"
                                 class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-3">
-                                <span>Kunjungi</span>
+                                <span>Coming Soon</span>
+                                <svg width="10" height="11" viewBox="0 0 10 11" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg" class="w-2.5 h-2.5">
+                                    <path d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001" stroke="currentColor"
+                                        stroke-linecap="square" />
+                                </svg>
+                            </a>
+                        </span>
+                        <span>
+                            Portfolio
+                            <a href="{{ url('/') }}" style="margin-left: 10px" target="_blank"
+                                class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-3">
+                                <span>Coming Soon</span>
+                                <svg width="10" height="11" viewBox="0 0 10 11" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg" class="w-2.5 h-2.5">
+                                    <path d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001" stroke="currentColor"
+                                        stroke-linecap="square" />
+                                </svg>
+                            </a>
+                        </span>
+                        <span>
+                            Portfolio
+                            <a href="{{ url('/') }}" style="margin-left: 10px" target="_blank"
+                                class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-3">
+                                <span>Coming Soon</span>
                                 <svg width="10" height="11" viewBox="0 0 10 11" fill="none"
                                     xmlns="http://www.w3.org/2000/svg" class="w-2.5 h-2.5">
                                     <path d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001" stroke="currentColor"
